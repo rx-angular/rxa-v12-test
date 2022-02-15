f7121ac9e0c67893b9393080c229837198ea27c9
Julian Jandl <julian@jandl.wien>
2022-02-15 23:56:51 +0100

    chore: install rxa, setup jest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { selectSlice } from '@rx-angular/state/selections';
+import { insert } from '@rx-angular/cdk/transformations';
+import { RxState } from '@rx-angular/state';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent extends RxState<{ test: string[] }> {
   title = 'v12-project';
+
+  state$ = this.select();
+  test$ = this.select().pipe(
+    selectSlice(['test']),
+    map(({ test }) => test)
+  );
+
+  constructor() {
+    super();
+    this.set({ test: insert(['baz'], 'foo') });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ForModule } from '@rx-angular/template/experimental/for';
+import { LetModule } from '@rx-angular/template/let';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LetModule,
+    ForModule
   ],
   providers: [],
   bootstrap: [AppComponent]
